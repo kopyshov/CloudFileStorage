ac7e4157c6ab46f58ec8f2aff1439a570e62322e
Dmitry Kopyshov <dkopysh@gmail.com>
2023-11-12 19:23:14 +0300

    Create data.sql

---
+++ src/main/java/com/kopyshov/cloudfilestorage/model/UserData.java
@@ -0,0 +1,49 @@
+package com.kopyshov.cloudfilestorage.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "user_data")
+public class UserData {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true, nullable = false)
+    private String username;
+    @Column(unique = true, nullable = false)
+    private String password;
+    @Column(unique = true, nullable = false)
+    private String name;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/resources/sql/data.sql
@@ -0,0 +1,1 @@
+INSERT INTO user_data (username, password, name) VALUES ('dima@ya.ru', '123', 'Dima');
