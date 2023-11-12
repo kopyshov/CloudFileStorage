6717d3546fe8877a0a1c67ef10edf729d43225da
Dmitry Kopyshov <dkopysh@gmail.com>
2023-11-12 19:30:26 +0300

    Create Role enum

---
+++ src/main/java/com/kopyshov/cloudfilestorage/database/entity/Role.java
@@ -0,0 +1,12 @@
+package com.kopyshov.cloudfilestorage.database.entity;
+
+import org.springframework.security.core.GrantedAuthority;
+
+public enum Role implements GrantedAuthority {
+    USER, ADMIN;
+
+    @Override
+    public String getAuthority() {
+        return name();
+    }
+}
